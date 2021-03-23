797401809031736758fa9f6ee2d5089fecda7e5a
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 18:45:27 -0400

    Added Title and Search bar to layout

--- src/layouts/core.js
+++ src/layouts/core.js
@@ -1,9 +1,12 @@
+import { Search } from '../components/core/search/Search';
+import { Titlebar } from '../components/core/titlebar/Titlebar';
 
 export const Layout = props => {
 
   return (
-    <div className="flex flex-col min-h-screen">
-
+    <div className="flex flex-col w-full min-h-screen">
+      <Titlebar />
+      <Search />
     </div>
   )
 
