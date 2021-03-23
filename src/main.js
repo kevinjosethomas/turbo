854e12b2d11f39c630e90deb85d4c97ca7d0d64a
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 19:33:53 -0400

    Added support for remote

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const electron = require('electron');
 
 const app = electron.app;
@@ -7,7 +8,17 @@ let mainWindow;
 
 function createWindow() {
 
-  mainWindow = new BrowserWindow({ show: false, frame: false, webPreferences: { webviewTag: true } });
+  mainWindow = new BrowserWindow({
+    show: false,
+    frame: false,
+    webPreferences: {
+      webviewTag: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+      enableRemoteModule: true
+    }
+  });
+
   mainWindow.loadURL('http://localhost:3000');
   mainWindow.maximize();
   mainWindow.removeMenu();
