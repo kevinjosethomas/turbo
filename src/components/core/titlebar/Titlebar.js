ff10641a5e3ee9239ba279494217113afbd51160
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 17:19:33 -0400

    Added basic Titlebar component

---
+++ src/components/core/titlebar/Titlebar.js
@@ -0,0 +1,16 @@
+
+export const Titlebar = props => {
+
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <></> {/* Add tab list component here */}
+      
+      <div className="flex flex-row items-center justify-end">
+        <img src="/assets/icons/material-minimize.svg" />
+        <img src="/assets/icons/material-close.svg" />
+        <img src="/assets/icons/material-close.svg" />
+      </div>
+    </div>
+  )
+
+}
