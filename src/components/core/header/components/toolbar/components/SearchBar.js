ebfdece419236122ebe3f2768bdde9145bef4cf4
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 13:44:32 -0400

    Half height

--- src/components/core/header/components/toolbar/components/SearchBar.js
+++ src/components/core/header/components/toolbar/components/SearchBar.js
@@ -2,7 +2,7 @@
 export const SearchBar = props => {
 
   return (
-    <div className="flex flex-row items-center justify-start w-10/12 px-4 bg-night-800 rounded-lg">
+    <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 bg-night-800 rounded-lg">
       <input
         className="w-full py-4 bg-transparent focus:outline-none text- text-gray-400 placeholder-gray-400"
         placeholder="Search Google or enter a URL"
