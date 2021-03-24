36cb904671c10a4091b89e649db34c9c9cd39af9
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 22:53:15 -0400

    Added div

--- src/components/core/header/Header.js
+++ src/components/core/header/Header.js
@@ -1,15 +1,13 @@
-import { Fragment } from 'react';
-
 import { TitleBar } from './components/titlebar/TitleBar';
 import { ToolBar } from './components/toolbar/ToolBar';
 
 export const Header = props => {
 
   return (
-    <Fragment>
+    <div className="flex flex-col items-start justify-start w-full h-28">
       <TitleBar window={props.window} />
       <ToolBar />
-    </Fragment>
+    </div>
   );
 
 }
