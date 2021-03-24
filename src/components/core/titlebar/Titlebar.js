dd633652e1f0884344197345b19888eeddd10ad9
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 22:36:45 -0400

    Minor Formatting Changes

--- src/components/core/titlebar/Titlebar.js
+++ src/components/core/titlebar/Titlebar.js
@@ -3,7 +3,7 @@ import { ActionBar } from "./components/ActionBar";
 export const Titlebar = props => {
 
   return (
-    <div className="flex flex-row items-center justify-between bg-night-800">
+    <div className="flex flex-row items-center justify-between bg-night-900">
       <br /> {/* Add tab list component here */}
       <ActionBar isMaximized={props.isMaximized} />
     </div>
