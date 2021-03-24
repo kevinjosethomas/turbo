3873a07c01634a1e666e2d18897b9679f92456f8
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 18:24:30 -0400

    Loaded tabbar

--- src/components/core/header/components/titlebar/Titlebar.js
+++ src/components/core/header/components/titlebar/Titlebar.js
@@ -1,4 +1,6 @@
-import { ActionBar } from "./components/ActionBar";
+import { TabBar } from './components/TabBar';
+import { ActionBar } from './components/ActionBar';
+
 
 export const TitleBar = props => {
 
@@ -11,6 +13,7 @@ export const TitleBar = props => {
   return (
     <div className="TitleBar flex flex-row items-center justify-between w-full pl-4 h-header-titlebar bg-night-mare">
       <div className="TabBar flex flex-row items-center justify-start">
+        <TabBar tablist={props.tablist} />
         <div
           className="flex flex-col items-center justify-center w-10 h-10 cursor-pointer text-night-sky-dusk hover:text-night-sky-noon bg-night-tab hover:bg-night-tab-active rounded transition duration-300"
           onClick={createTab}
