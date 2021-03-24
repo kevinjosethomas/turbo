8d90119c6ee452a79f3111c25a92c72d4e65caec
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 13:44:16 -0400

    Added Buttons navigation

--- src/components/core/header/components/toolbar/Toolbar.js
+++ src/components/core/header/components/toolbar/Toolbar.js
@@ -1,9 +1,11 @@
+import { Buttons } from './components/Buttons';
 import { SearchBar } from './components/SearchBar';
 
 export const ToolBar = props => {
 
   return (
-    <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 py-2 bg-night-tab">
+    <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 bg-night-tab">
+      <Buttons />
       <SearchBar />
     </div>
   )
