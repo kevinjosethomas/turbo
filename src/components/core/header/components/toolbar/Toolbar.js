f3b27c0f2d46a95feff9fcc7db2b6df94bb2c4a7
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 22:43:17 -0400

    Renamed Toolbar

--- src/components/core/header/components/toolbar/Toolbar.js
+++ src/components/core/header/components/toolbar/Toolbar.js
@@ -1,6 +1,6 @@
-import { Search } from './components/Search';
+import { SearchBar } from './components/SearchBar';
 
-export const Toolbar = props => {
+export const ToolBar = props => {
 
   return (
     <div className="flex flex-row items-center justify-start w-full px-4 py-2 bg-night-700">
