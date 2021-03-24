29b042fea57f95ad9e851d5a56fdba8ce3a6c720
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 13:59:19 -0400

    Added Controls component

--- src/components/core/header/components/toolbar/Toolbar.js
+++ src/components/core/header/components/toolbar/Toolbar.js
@@ -1,11 +1,15 @@
-import { Buttons } from './components/Buttons';
+import { Controls } from './components/Controls';
 import { SearchBar } from './components/SearchBar';
 
 export const ToolBar = props => {
 
   return (
     <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 bg-night-tab">
-      <Buttons />
+      <Controls
+        onBackword={() => void(0)}
+        onForward={() => void(0)}
+        onReload={() => void(0)}
+      />
       <SearchBar />
     </div>
   )
