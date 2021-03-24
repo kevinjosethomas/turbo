09370c69ddaa8ff37e3446d1cd5ee7eda4d8803d
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 12:39:09 -0400

    Added titlebar color and height

--- src/components/core/header/components/titlebar/Titlebar.js
+++ src/components/core/header/components/titlebar/Titlebar.js
@@ -3,7 +3,7 @@ import { ActionBar } from "./components/ActionBar";
 export const TitleBar = props => {
 
   return (
-    <div className="TitleBar flex flex-row items-center justify-between w-full bg-night-900">
+    <div className="TitleBar flex flex-row items-center justify-between w-full h-header-titlebar bg-night-mare">
       <br /> {/* Add tab list component here */}
       <ActionBar isMaximized={props.isMaximized} />
     </div>
