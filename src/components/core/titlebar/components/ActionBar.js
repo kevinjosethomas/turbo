7c04d144f075b65a864c98c1c714020713002249
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 22:19:03 -0400

    Added custom icon colors

--- src/components/core/titlebar/components/ActionBar.js
+++ src/components/core/titlebar/components/ActionBar.js
@@ -16,11 +16,13 @@ export const ActionBar = props => {
 export const Action = props => {
 
   let icon;
+  const iconColor = "#D1D5DB";
   switch (props.type) {
     case "close":
       icon = (
         <svg width="12" height="12" viewBox="0 0 12 12">
           <path
+            fill={iconColor}
             d="m6.8496 6 5.1504 5.1504-0.84961 0.84961-5.1504-5.1504-5.1504 5.1504-0.84961-0.84961 5.1504-5.1504-5.1504-5.1504 0.84961-0.84961 5.1504 5.1504 5.1504-5.1504 0.84961 0.84961z"
             strokeWidth=".3"
           />
@@ -30,7 +32,7 @@ export const Action = props => {
     case "restore":
       icon = (
         <svg width="10" height="10" viewBox="0 0 10 10">
-          <path d="m10-1.6667e-6v10h-10v-10zm-1.001 1.001h-7.998v7.998h7.998z" strokeWidth=".25" />
+          <path fill={iconColor} d="m10-1.6667e-6v10h-10v-10zm-1.001 1.001h-7.998v7.998h7.998z" strokeWidth=".25" />
         </svg>
       );
       break;
@@ -39,7 +41,7 @@ export const Action = props => {
         <svg width="11" height="11" viewBox="0 0 11 11">
           <path
             d="m11 8.7978h-2.2021v2.2022h-8.7979v-8.7978h2.2021v-2.2022h8.7979zm-3.2979-5.5h-6.6012v6.6011h6.6012zm2.1968-2.1968h-6.6012v1.1011h5.5v5.5h1.1011z"
-            strokeWidth=".275"
+            fill={iconColor} strokeWidth=".275"
           />
         </svg>
       );
@@ -47,7 +49,7 @@ export const Action = props => {
     case "minimize":
       icon = (
         <svg width="11" height="1" viewBox="0 0 11 1">
-          <path d="m11 0v1h-11v-1z" strokeWidth=".26208"/>
+          <path fill={iconColor} d="m11 0v1h-11v-1z" strokeWidth=".26208"/>
         </svg>
       );
       break;
@@ -55,7 +57,7 @@ export const Action = props => {
 
   return (
     <div
-      className="flex flex-row items-center justify-center h-10 w-10 hover:bg-night-700"
+      className={`flex flex-row items-center justify-center h-10 w-10 cursor-pointer ${props.type === 'close'? 'hover:bg-red-800' : 'hover:bg-night-700'}`}
       onClick={() => window.ipcRenderer.send('window-change', props.type)}
     >
       {icon}
