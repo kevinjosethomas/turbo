887c81c4c2bdd8133c20217804f76d8f0bba5c6a
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 21:29:10 -0400

    Updated Layout settings

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useEffect } from "react";
-
 import './styles/tailwind.css'
 import { CoreLayout } from "./layouts/core";
 import { View } from './components/core/View';
 
 const App = () => {
 
-  let remote, mainWindow;
-  useEffect(() => {
-    remote = window.electron.remote;
-    mainWindow = remote.getCurrentWindow();
-  }, [])
-
   return (
-    <CoreLayout window={mainWindow}>
+    <CoreLayout>
       <View id="1" />
     </CoreLayout>
   )
