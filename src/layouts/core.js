ec0221804b64caa1b92ee6a49ab6b9c6338653a0
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 21:27:02 -0400

    Added temporary background color

--- src/layouts/core.js
+++ src/layouts/core.js
@@ -4,7 +4,7 @@ import { Titlebar } from '../components/core/titlebar/Titlebar';
 export const CoreLayout = props => {
 
   return (
-    <div className="flex flex-col w-full min-h-screen">
+    <div className="flex flex-col w-full min-h-screen bg-night-600">
       <Titlebar window={props.window} />
       <Search />
     </div>
