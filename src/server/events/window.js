c6f6270527e95e6f3d9a028e318d0e54c1ec8f1f
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 16:30:55 -0400

    Removed event handlers

--- src/server/events/window.js
+++ src/server/events/window.js
@@ -1,13 +1,8 @@
 
 exports.windowEventHandler = (win) => {
 
-  win.webContents.on('minimize', () => {
-    console.log('minimize')
-  })
-
-  win.webContents.on('resize', () => {
-    console.log('resized')
-    win.webContents.send('receive-window-maximized', win.isMaximized());
+  win.on('resize', () => {
+     win.webContents.send('receive-window-maximized', win.isMaximized());
   })
 
 }
