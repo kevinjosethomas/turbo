48689ba2207dec692b554770eecd60e286b1c3ff
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 12:47:41 -0400

    Added SVG icon

---
+++ src/components/core/header/components/toolbar/components/Buttons.js
@@ -0,0 +1,15 @@
+
+export const Buttons = props => {
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <div className="flex flex-row items-center justify-center h-1/2">
+
+      </div>
+      <div className="flex flex-row items-center justify-center h-1/2">
+        
+      </div>
+    </div>
+  );
+
+}
