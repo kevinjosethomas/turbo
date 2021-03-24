afbd47064320981f59f0fe402c541db9fbce9299
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 13:44:22 -0400

    Removed SVG images

--- src/components/core/header/components/toolbar/components/Buttons.js
+++ src/components/core/header/components/toolbar/components/Buttons.js
@@ -2,13 +2,23 @@
 export const Buttons = props => {
 
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex flex-row items-center justify-center h-1/2">
-
-      </div>
-      <div className="flex flex-row items-center justify-center h-1/2">
-        
-      </div>
+    <div className="flex flex-row items-center justify-center h-full">
+      <i className="far fa-arrow-right" />
+      {/* <img
+        src={toolbarBack}
+        draggable="false"
+        onClick={props.onClick}
+      />
+      <img
+        src={toolbarForward}
+        draggable="false"
+        onClick={props.onClick}
+      />
+      <img
+        src={toolbarReload}
+        draggable="false"
+        onClick={props.onClick}
+      /> */}
     </div>
   );
 
