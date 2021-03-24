b415b813b2c267be71a44e5123cfc72c7cc5eee9
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 22:41:54 -0400

    Added Header

--- src/layouts/core.js
+++ src/layouts/core.js
@@ -1,12 +1,10 @@
-import { Toolbar } from '../components/core/toolbar/Toolbar';
-import { Titlebar } from '../components/core/titlebar/Titlebar';
+import { Header } from '../components/core/header/Header';
 
 export const CoreLayout = props => {
 
   return (
     <div className="flex flex-col w-full min-h-screen bg-night-600">
-      <Titlebar window={props.window} />
-      <Toolbar />
+      <Header />
     </div>
   )
 
