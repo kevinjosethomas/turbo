9491cd815444038902252096396944ff21497ed0
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-23 21:27:28 -0400

    Removed webview

--- src/components/core/View.js
+++ src/components/core/View.js
@@ -4,11 +4,6 @@ export const View = props => {
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <webview
-        id={props.id}
-        className="w-full h-screen"
-        src="https://google.com/"
-      />
     </div>
   )
 
