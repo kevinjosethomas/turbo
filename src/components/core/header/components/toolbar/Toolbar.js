41515f9e4e6395c82bed9d11164c574ff24b0a0d
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 12:39:14 -0400

    Added toolbar color and height

--- src/components/core/header/components/toolbar/Toolbar.js
+++ src/components/core/header/components/toolbar/Toolbar.js
@@ -3,7 +3,7 @@ import { SearchBar } from './components/SearchBar';
 export const ToolBar = props => {
 
   return (
-    <div className="flex flex-row items-center justify-start w-full px-4 py-2 bg-night-700">
+    <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 py-2 bg-night-tab">
       <SearchBar />
     </div>
   )
