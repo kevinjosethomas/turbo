51d3ca47ebbe38d09212749be8d660e90c42af31
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 09:06:23 -0400

    Added isMaximized props

--- src/components/core/header/Header.js
+++ src/components/core/header/Header.js
@@ -5,7 +5,7 @@ export const Header = props => {
 
   return (
     <div className="flex flex-col items-start justify-start w-full h-28">
-      <TitleBar window={props.window} />
+      <TitleBar isMaximized={props.isMaximized} />
       <ToolBar />
     </div>
   );
