0efd08394a9dacbd44b409ce38512add51f5d415
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-25 15:05:35 -0400

    Added support to type in search bar

--- src/components/core/header/components/toolbar/components/SearchBar.js
+++ src/components/core/header/components/toolbar/components/SearchBar.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 
 export const SearchBar = props => {
 
   const activeTab = props.tablist[props.tablist.findIndex(el => el.active === true)];
+  const [searchURL, setSearchURL] = useState(activeTab ? activeTab.url : '');
+  const handleChange = (event) => {
+    setSearchURL(event.target.value);
+  }
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      window.ipcRenderer.send('')
+    }
+  }
 
   return (
     <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 bg-night-tab-active rounded">
       <input
         className="w-full py-4 bg-transparent focus:outline-none text-gray-400 placeholder-gray-500"
         placeholder="Search or enter a URL"
-        value={activeTab ? activeTab.url : ''}
+        value={searchURL}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
       />
     </div>
   )
