2b67262a153ba40c86347570fb3560a99864630d
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 21:21:40 -0400

    Added functions for tab titles and tab favicons

--- src/server/models/tablist.js
+++ src/server/models/tablist.js
@@ -1,24 +1,40 @@
+const { settings } = require('../data/settings');
 
 class Tablist {
 
   constructor(win) {
     this.win = win;
     this.tablist = [];
+    this.activeView = null;
     this.friendlyTablist = [];
   }
 
-  push({ url, view, id }) {
+  push({ url, view, id, favicon }) {
     this.tablist.push({
       id: id,
       url: url,
       view: view,
+      favicon: favicon,
       active: false
     });
     this.friendlyTablist.push({
       id: id,
       url: url,
-      active: false
+      active: false,
+      favicon: favicon,
     })
+  }
+
+  setTabTitle(id, title) {
+    const index = this.tablist.findIndex(el => el.id === id);
+    this.tablist[index].title = title;
+    this.friendlyTablist[index].title = title;
+  }
+
+  setTabFavicon(id, favicon) {
+    const index = this.tablist.findIndex(el => el.id === id);
+    this.tablist[index].favicon = favicon;
+    this.friendlyTablist[index].favicon = favicon;
   }
 
   setActiveTab(id) {
@@ -26,16 +42,39 @@ class Tablist {
       if (item.id === id) {
         this.tablist[index].active = true;
         this.friendlyTablist[index].active = true;
+
+        const windowSize = this.win.getSize();
+        this.tablist[index].view.setBounds({
+          x: 0,
+          y: settings.minHeight,
+          width: windowSize[0],
+          height: windowSize[1] - settings.minHeight
+        });
+        this.activeView = this.tablist[index].view;
       } else if (item.active && item.id !== id) {
         this.tablist[index].active = false;
         this.friendlyTablist[index].active = false;
+        this.activeView = null;
+        this.tablist[index].view.setBounds({
+          x: 0,
+          y: 0,
+          width: 0,
+          height: 0
+        });
       }
     });
   }
 
   closeTab(id) {
-    this.tablist = this.tablist.filter(el => el.id !== id);
-    this.friendlyTablist = this.friendlyTablist.filter(el => el.id !== id);
+    const index = this.tablist.findIndex(el => el.id === id);
+    if (index >= 0) {
+      this.win.removeBrowserView(this.tablist[index].view);
+      this.tablist.splice(index, 1);
+      this.friendlyTablist.splice(index, 1);
+      if (this.tablist.length) {
+        this.setActiveTab(this.tablist[index > 0 ? index - 1 : 0].id)
+      }
+    }
   }
 
 }
