7c3c62e65a4165f2c3072fcb89d626d7fdfb0dc4
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-25 19:25:04 -0400

    Minor UX Changes

--- src/components/core/header/components/toolbar/components/SearchBar.js
+++ src/components/core/header/components/toolbar/components/SearchBar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export const SearchBar = props => {
 
@@ -6,16 +6,19 @@ export const SearchBar = props => {
   const [activeURL, setActiveURL] = useState(activeTab ? activeTab.url : '');
   const [searchURL, setSearchURL] = useState(activeTab ? activeTab.url : '');
 
+  const searchRef = useRef(null);
+
   if (activeTab && activeTab.url !== activeURL) {
     setActiveURL(activeTab.url);
     setSearchURL(activeTab.url);
+    searchRef.current.blur();
   }
 
   const handleChange = (event) => {
     setSearchURL(event.target.value);
   }
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter'  && searchURL) {
       window.ipcRenderer.send('set-active-tab-url', {
         id: activeTab.id,
         url: searchURL
@@ -27,10 +30,12 @@ export const SearchBar = props => {
     <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 bg-night-tab-active rounded">
       <input
         className="w-full py-4 bg-transparent focus:outline-none text-gray-400 placeholder-gray-500"
+        ref={searchRef}
         placeholder="Search or enter a URL"
         value={searchURL}
         onChange={handleChange}
         onKeyPress={handleKeyPress}
+        onClick={event => event.target.select()}
       />
     </div>
   )
