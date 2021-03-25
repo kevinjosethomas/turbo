e165582dd5b6bdf3f7accf08d5bbb9a0e62f7872
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-25 11:31:08 -0400

    Fixed padding

--- src/components/core/header/components/titlebar/Titlebar.js
+++ src/components/core/header/components/titlebar/Titlebar.js
@@ -5,7 +5,7 @@ import { ActionBar } from './components/ActionBar';
 export const TitleBar = props => {
 
   return (
-    <div className="TitleBar flex flex-row items-center justify-between w-full pl-4 h-header-titlebar bg-night-mare">
+    <div className="TitleBar flex flex-row items-center justify-between w-full h-header-titlebar bg-night-mare">
       <TabBar tablist={props.tablist} />
       <ActionBar isMaximized={props.isMaximized} />
     </div>
