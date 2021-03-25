3a6747829f803429f10c4228cc90e6729e2ef0bf
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-25 10:12:05 -0400

    Added NewTab component

--- src/components/core/header/components/titlebar/components/TabBar.js
+++ src/components/core/header/components/titlebar/components/TabBar.js
@@ -29,20 +29,13 @@ export const TabBar = props => {
           />
         ))}
       </div>
-      <div
-        className="flex flex-col items-center justify-center w-8 h-8 cursor-pointer text-night-sky-dusk hover:text-night-sky-noon bg-night-tab hover:bg-night-tab-active rounded transition duration-300"
-        onClick={createTab}
-      >
-        <i className="far fa-plus text-lg" />
-      </div>
+      <NewTab onClick={createTab} />
     </div>
   );
 
 }
 
 export const Tab = props => {
-
-  console.log(props.suffix)
 
   return (
     <div
@@ -71,4 +64,16 @@ export const Tab = props => {
     </div>
   )
 
+}
+
+export const NewTab = props => {
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center w-8 h-8 cursor-pointer text-night-sky-dusk hover:text-night-sky-noon bg-night-tab hover:bg-night-tab-active rounded transition duration-300"
+      onClick={props.onClick}
+    >
+      <i className="far fa-plus text-lg" />
+    </div>
+  );
 }
