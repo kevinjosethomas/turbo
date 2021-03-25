567e3cd35e4a83c9d727a23fdd486b64c8b7d797
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-24 23:41:06 -0400

    Added top toolbar border

--- src/components/core/header/components/toolbar/Toolbar.js
+++ src/components/core/header/components/toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import { SearchBar } from './components/SearchBar';
 export const ToolBar = props => {
 
   return (
-    <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 bg-night-tab">
+    <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 bg-night-tab border-t-2 border-night-tab-active">
       <Controls
         onBackword={() => void(0)}
         onForward={() => void(0)}
