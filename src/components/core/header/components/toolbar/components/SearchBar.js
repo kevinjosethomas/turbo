a9e0fa13c8c60326f7ad75046240cfe2338be087
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-28 17:17:39 -0400

    Added search engine logo

--- src/components/core/header/components/toolbar/components/SearchBar.js
+++ src/components/core/header/components/toolbar/components/SearchBar.js
@@ -1,5 +1,10 @@
 import { useState, useRef } from 'react';
 
+import Bing from '../../../../../../assets/images/engines/bing.svg';
+import DuckDuckGo from '../../../../../../assets/images/engines/duckduckgo.svg';
+import Google from '../../../../../../assets/images/engines/google.svg';
+import Yahoo from '../../../../../../assets/images/engines/yahoo.svg';
+
 export const SearchBar = props => {
 
   const activeTab = props.tablist[props.tablist.findIndex(el => el.active === true)];
@@ -33,7 +38,8 @@ export const SearchBar = props => {
   }
 
   return (
-    <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 bg-night-tab-active rounded">
+    <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 space-x-4 bg-night-tab-active rounded">
+      <img src={Google} />
       <input
         className="w-full py-4 bg-transparent focus:outline-none text-gray-400 placeholder-gray-500"
         ref={searchRef}
