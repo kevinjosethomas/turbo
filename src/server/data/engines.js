05542a293b1cb7d77a261991d43bc0d832cafce0
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-28 19:48:02 -0400

    Added search engine list

---
+++ src/server/data/engines.js
@@ -0,0 +1,31 @@
+import Bing from '../../assets/images/engines/bing.svg'
+import DuckDuckGo from '../../assets/images/engines/duckduckgo.svg'
+import Google from '../../assets/images/engines/google.svg'
+import Yahoo from '../../assets/images/engines/yahoo.svg'
+
+export const engines = [
+  {
+    id: 1,
+    name: 'bing',
+    url: 'https://google.com',
+    icon: Bing
+  },
+  {
+    id: 2,
+    name: 'duckduckgo',
+    url: 'https://google.com',
+    icon: DuckDuckGo
+  },
+  {
+    id: 3,
+    name: 'google',
+    url: 'https://google.com',
+    icon: Google
+  },
+  {
+    id: 4,
+    name: 'yahoo',
+    url: 'https://google.com',
+    icon: Yahoo
+  }
+]
