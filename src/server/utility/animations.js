9ff5e655e5dd2d8c11ccb51d74f6f7027a5b4c39
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-29 19:01:46 -0400

    Added animations file

---
+++ src/server/utility/animations.js
@@ -0,0 +1,14 @@
+
+export const searchEngineSlide = {
+  animate: {
+    width: 0,
+    transition: { duration: 1 }
+  },
+  initial: {
+    width: "auto"
+  },
+  exit: {
+    width: 0,
+    transition: { duration: 1 }
+  }
+}
