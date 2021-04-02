26766ac69726badfaec9d828768ca8219593b9fc
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-02 15:09:18 -0400

    Added separate file for ES6 engine icons

--- src/server/data/engines.js
+++ src/server/data/engines.js
@@ -1,39 +1,31 @@
-import Bing from '../../assets/images/engines/bing.svg'
-import DuckDuckGo from '../../assets/images/engines/duckduckgo.svg'
-import Google from '../../assets/images/engines/google.svg'
-import Yahoo from '../../assets/images/engines/yahoo.svg'
 
-export const engines = [
+exports.engines = [
   {
     id: 1,
     name: 'bing',
     label: 'Bing',
     url: 'https://bing.com',
-    searchURL: 'https://bing.com/search?q=',
-    icon: Bing
+    searchURL: 'https://bing.com/search?q='
   },
   {
     id: 2,
     name: 'duckduckgo',
     label: 'DuckDuckGo',
     url: 'https://duckduckgo.com',
-    searchURL: 'https://duckduckgo.com/?q',
-    icon: DuckDuckGo
+    searchURL: 'https://duckduckgo.com/?q='
   },
   {
     id: 3,
     name: 'google',
     label: 'Google',
     url: 'https://google.com',
-    searchURL: 'https://google.com/?q',
-    icon: Google
+    searchURL: 'https://google.com/?q='
   },
   {
     id: 4,
     name: 'yahoo',
     label: 'Yahoo',
     url: 'https://yahoo.com',
-    searchURL: 'https://search.yahoo.com/?q',
-    icon: Yahoo
+    searchURL: 'https://search.yahoo.com/?q='
   }
-]
+];
