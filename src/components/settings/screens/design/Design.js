a22e9e032f4c976707999563e0f16ce46300feff
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 14:11:48 -0400

    Added hexes array and cleaned up code

--- src/components/settings/screens/design/Design.js
+++ src/components/settings/screens/design/Design.js
@@ -2,17 +2,49 @@ import { Hex } from './components/Hex';
 
 export const Design = props => {
 
+  const hexes = [
+    {
+      id: 1,
+      label: 'Really Dark Background'
+    },
+    {
+      id: 2,
+      label: 'Really Dark Text'
+    },
+    {
+      id: 3,
+      label: 'Slightly Dark Background'
+    },
+    {
+      id: 4,
+      label: 'Pretty Dark Text'
+    },
+    {
+      id: 5,
+      label: 'Moody Background'
+    },
+    {
+      id: 6,
+      label: 'Not Light Text'
+    },
+  ]
+
+  const changeHex = (id, hex) => {
+    console.log('changing hex')
+    console.log(hex)
+  }
+
   return (
-    <div className="flex flex-row items-start justify-start px-10 w-full h-full space-x-12">
-      <div className="flex flex-col items-start justify-start space-y-6">
-        <Hex label="Really Dark Backgrounds" />
-        <Hex label="Slightly Dark Backgrounds" />
-        <Hex label="Moody Backgrounds" />
-      </div>
-      <div className="flex flex-col items-start justify-start space-y-6">
-        <Hex label="Really Dark Text" />
-        <Hex label="Pretty Dark Text" />
-        <Hex label="Not Light Text" />
+    <div className="flex flex-row items-start justify-start px-10 w-full h-full">
+      <div className="flex flex-row items-start justify-start flex-wrap max-w-2xl">
+        { hexes.map(hex => (
+          <Hex
+            key={hex.id}
+            id={hex.id}
+            label={hex.label}
+            changeHex={changeHex}
+          />
+        ))}
       </div>
     </div>
   );
