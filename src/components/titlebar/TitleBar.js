3a33ea6aeb7bab55bbc5a9f1e8bc6ce48046f69a
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-04 20:37:55 -0400

    Renamed TitleBar to Titlebar

--- src/components/titlebar/TitleBar.js
+++ src/components/titlebar/TitleBar.js
@@ -1,8 +1,7 @@
 import { TabBar } from './components/TabBar';
 import { ActionBar } from './components/ActionBar';
 
-
-export const TitleBar = props => {
+export const Titlebar = props => {
 
   return (
     <div className="TitleBar flex flex-row items-center justify-between w-full h-header-titlebar bg-night-mare">
--- src/layouts/browser.js
+++ src/layouts/browser.js
@@ -1,12 +1,12 @@
 import { Toolbar } from '../components/toolbar/Toolbar';
-import { TitleBar } from '../components/titlebar/TitleBar';
+import { Titlebar } from '../components/titlebar/Titlebar';
 
 export const Browser = props => {
 
   return (
     <div className="flex flex-col w-full min-h-screen bg-night-600">
       <div className="flex flex-col items-start justify-start w-full h-32">
-        <TitleBar tablist={props.tablist} isMaximized={props.isMaximized} />
+        <Titlebar tablist={props.tablist} isMaximized={props.isMaximized} />
         <Toolbar tablist={props.tablist} />
       </div>
     </div>
