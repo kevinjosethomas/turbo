d2f7506fa00de8480d012a476748618f3b9347f7
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 14:26:21 -0400

    Added default color values

--- src/server/data/settings.js
+++ src/server/data/settings.js
@@ -3,6 +3,14 @@ exports.settings = {
   path: {
     settings: '/settings'
   },
+  colors: {
+    "--color-nightmare": "#141414",
+    "--color-night-tab": "#1c1c1c",
+    "--color-night-tab-active": "#282828",
+    "--color-night-sky-dawn": "#e0e0e0",
+    "--color-night-sky-noon": "#bdbdbd",
+    "--color-night-sky-dusk": "#828282"
+  },
   size: {
     window: {
       min: {
