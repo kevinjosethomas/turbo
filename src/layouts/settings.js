8af573588802a0814ac5807b8c19182c8df0f6ef
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 13:26:09 -0400

    Added header

--- src/layouts/settings.js
+++ src/layouts/settings.js
@@ -12,8 +12,10 @@ export const Settings = props => {
         activeSettingScreen={activeSettingScreen}
         setActiveSettingScreen={setActiveSettingScreen}
       />
-      <div className="flex flex-col items-center justify-center w-max">
-
+      <div className="flex flex-col items-center justify-center w-9/12">
+        <div className="flex flex-row items-center justify-between px-10 w-full">
+          <span className="pl-4 font-semibold text-4xl text-night-sky-dawn">{ activeSettingScreen }</span>
+        </div>
       </div>
     </div>
   )
