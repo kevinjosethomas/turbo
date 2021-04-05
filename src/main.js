ea435b66986a37bba21b61477eacfa236a30cd2b
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 11:34:17 -0400

    Prettification

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 const {
   app,
   ipcMain,
-  BrowserView,
   BrowserWindow
 } = require('electron');
 const url = require('url');
 const path = require('path');
 const Store = require('electron-store');
-const electronLocalshortcut = require('electron-localshortcut');
 
 const { settings } = require('./server/data/settings');
 const { Tablist } = require('./server/models/tablist');
@@ -15,20 +13,11 @@ const { ipcEventHandler } = require('./server/handlers/ipc');
 const { windowEventHandler } = require('./server/handlers/window');
 const { keyboardEventHandler } = require('./server/handlers/keyboard');
 
-// Creates global window and store objects
 let win;
-const store = new Store({
-  tabHistory: {
-    type: "array",
-    items: {
-      type: "object"
-    }
-  }
-});
+const store = new Store();
 
 const createWindow = () => {
 
-  // Create BrowserWindow instance
   win = new BrowserWindow({
     show: false,
     frame: false,
@@ -41,15 +30,13 @@ const createWindow = () => {
     }
   });
 
-  // Load React application
   const reactURL = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, '../build/index.html'),
     protocol: 'file:',
     slashes: true
   });
-  win.loadURL(reactURL);
-  win.maximize();
-  win.show();
+  win.loadURL(reactURL).maximize().show();
+  win.webContents.openDevTools();
 
   const modal = new BrowserWindow({
     parent: win,
@@ -64,15 +51,12 @@ const createWindow = () => {
   modal.loadURL('http://localhost:3000/settings')
   modal.webContents.openDevTools();
 
-  let tablist = new Tablist(win);
-
-  win.webContents.openDevTools();
+  const tablist = new Tablist(win);
 
   ipcEventHandler(win, { ipcMain: ipcMain, tablist: tablist, store: store });
   windowEventHandler(win);
   keyboardEventHandler(win, { tablist: tablist });
 
-  // Null window on close
   win.on('closed', () => {
     win = null;
   });
@@ -81,7 +65,13 @@ const createWindow = () => {
 
 app.on('ready', createWindow);
 
-app.on('activate', () => win === null ? createWindow : void(0));
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  } else {
+    return;
+  }
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
