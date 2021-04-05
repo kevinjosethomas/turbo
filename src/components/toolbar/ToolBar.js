4e44f65da16895fce2ae6718242649fb910a3de7
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-04 20:37:19 -0400

    Renamed ToolBar to toolbar

--- src/components/toolbar/ToolBar.js
+++ src/components/toolbar/ToolBar.js
@@ -1,7 +1,7 @@
 import { Controls } from './components/Controls';
 import { SearchBar } from './components/SearchBar';
 
-export const ToolBar = props => {
+export const Toolbar = props => {
 
   return (
     <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 bg-night-tab border-t-2 border-night-tab-active">
--- src/layouts/browser.js
+++ src/layouts/browser.js
@@ -1,4 +1,4 @@
-import { ToolBar } from '../components/toolbar/ToolBar';
+import { Toolbar } from '../components/toolbar/Toolbar';
 import { TitleBar } from '../components/titlebar/TitleBar';
 
 export const Browser = props => {
@@ -7,7 +7,7 @@ export const Browser = props => {
     <div className="flex flex-col w-full min-h-screen bg-night-600">
       <div className="flex flex-col items-start justify-start w-full h-32">
         <TitleBar tablist={props.tablist} isMaximized={props.isMaximized} />
-        <ToolBar tablist={props.tablist} />
+        <Toolbar tablist={props.tablist} />
       </div>
     </div>
   )
