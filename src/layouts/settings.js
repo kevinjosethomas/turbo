2b19ac0db55e08c661228938724b66475b8d44cf
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 13:19:50 -0400

    Add activeSettingScreen

--- src/layouts/settings.js
+++ src/layouts/settings.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 import { Sidebar } from '../components/settings/Sidebar';
 
 export const Settings = props => {
 
+  const [activeSettingScreen, setActiveSettingScreen] = useState('Design');
+
   return (
     <div className="flex flex-row items-center justify-between w-screen h-screen bg-night-mare">
-      <Sidebar />
+      <Sidebar
+        activeSettingScreen={activeSettingScreen}
+        setActiveSettingScreen={setActiveSettingScreen}
+      />
       <div className="flex flex-col items-center justify-center w-max">
 
       </div>
