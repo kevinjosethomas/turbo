cdc9cb84e45e0ce996fa725290af432d4d41203c
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-05 12:09:31 -0400

    Cleaned up settings data

--- src/server/data/settings.js
+++ src/server/data/settings.js
@@ -1,6 +1,24 @@
 
 exports.settings = {
-  minWidth: 300,
-  minHeight: 128,
-  settingsPath: '/settings'
+  path: {
+    settings: '/settings'
+  },
+  size: {
+    window: {
+      min: {
+        width: 300,
+        height: 128
+      }
+    },
+    modal: {
+      min: {
+        width: 500,
+        height: 500
+      },
+      default: {
+        width: 0.9,
+        height: 0.8
+      }
+    }
+  }
 }
