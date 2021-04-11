7bdbac2a375286f7830ba527bf7a35f5e80c5a77
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-11 19:05:08 -0400

    Updated user agent

--- src/server/data/settings.js
+++ src/server/data/settings.js
@@ -1,7 +1,7 @@
 exports.settings = {
   core: {
     userAgent:
-      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/87.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
   },
   path: {
     settings: "/settings",
