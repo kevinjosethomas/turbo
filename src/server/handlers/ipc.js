9a13e4562abce655fee1701ee5d0f0d6b60c99dc
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-10 22:07:52 -0400

    Added prettier and support for google sign in

--- src/server/handlers/ipc.js
+++ src/server/handlers/ipc.js
@@ -1,93 +1,93 @@
-const { BrowserView } = require('electron');
-const buildChromeContextMenu = require('electron-chrome-context-menu').default;
+const { BrowserView } = require("electron");
+const buildChromeContextMenu = require("electron-chrome-context-menu").default;
 
-const { engines } = require('../data/engines');
-const { checkURL } = require('../utility/methods');
+const { engines } = require("../data/engines");
+const { settings } = require("../data/settings");
+const { checkURL } = require("../utility/methods");
 
 exports.ipcEventHandler = (win, util) => {
-
-  const {
-    ipcMain,
-    tablist,
-    modal,
-    store
-  } = util;
+  const { ipcMain, tablist, modal, store } = util;
 
   const pushHistory = (url) => {
-    let history = store.get('history');
+    let history = store.get("history");
     history = history ? history : [];
 
     history.push({
       url: url,
-      time: new Date()
+      time: new Date(),
     });
-    store.set('history', history);
-  }
+    store.set("history", history);
+  };
 
-  ipcMain.on('request-window-maximized', (event) => {
-    event.reply('receive-window-maximized', win.isMaximized());
-  })
-  
-  ipcMain.on('window-change', (_, type) => {
+  ipcMain.on("request-window-maximized", (event) => {
+    event.reply("receive-window-maximized", win.isMaximized());
+  });
+
+  ipcMain.on("window-change", (_, type) => {
     switch (type) {
-      case 'minimize':
+      case "minimize":
         win.minimize();
         break;
-      case 'maximize':
+      case "maximize":
         win.maximize();
         break;
-      case 'restore':
+      case "restore":
         win.unmaximize();
         win.setSize(800, 600);
-        break; 
-      case 'close':
+        break;
+      case "close":
         win.close();
     }
   });
 
-  ipcMain.on('open-modal', _ => {
+  ipcMain.on("open-modal", (_) => {
     modal.show();
-  })
-
-  ipcMain.on('close-modal', _ => {
-    modal.hide();
-  })
-
-  ipcMain.on('request-colors', (event) => {
-    event.reply('receive-colors', store.get('design.colors'));
-  })
-
-  ipcMain.on('change-color', (event, { name, hex }) => {
-    store.set(`design.colors.${name}`, hex);
-    event.reply('receive-colors', store.get('design.colors'));
-  })
-
-  ipcMain.on('request-tabs', event => {
-    event.reply('receive-tabs', tablist.friendlyTablist);
   });
 
-  ipcMain.on('create-tab', (event, openURL) => {
-    
+  ipcMain.on("close-modal", (_) => {
+    modal.hide();
+  });
+
+  ipcMain.on("request-colors", (event) => {
+    event.reply("receive-colors", store.get("design.colors"));
+  });
+
+  ipcMain.on("change-color", (event, { name, hex }) => {
+    store.set(`design.colors.${name}`, hex);
+    event.reply("receive-colors", store.get("design.colors"));
+  });
+
+  ipcMain.on("request-tabs", (event) => {
+    event.reply("receive-tabs", tablist.friendlyTablist);
+  });
+
+  ipcMain.on("create-tab", (event, openURL) => {
     let url;
-    let favicon = title = null;
+    let favicon = (title = null);
 
     if (openURL) {
       if (checkURL(openURL)) {
-        if (!openURL.startsWith('http://') && !openURL.startsWith('https://')) {
-          url = `https://${openURL}`
+        if (!openURL.startsWith("http://") && !openURL.startsWith("https://")) {
+          url = `https://${openURL}`;
         } else {
           url = openURL;
         }
       } else {
-        url = `https://google.com/search?q=${openURL.replace(' ', '+')}`
+        url = `https://google.com/search?q=${openURL.replace(" ", "+")}`;
       }
     } else {
-      url = 'https://google.com/'
+      url = "https://google.com/";
     }
-    
+
     const view = new BrowserView();
     const id = view.webContents.id;
-    view.webContents.loadURL(url)
+    const parsedUrl = new URL(url);
+    const userAgent = parsedUrl.hostname.includes("google.com")
+      ? settings.core.userAgent
+      : view.webContents.getUserAgent();
+
+    view.webContents.loadURL(url, { userAgent: userAgent });
+    console.log(view.webContents.userAgent);
     win.addBrowserView(view);
 
     tablist.push({
@@ -95,92 +95,90 @@ exports.ipcEventHandler = (win, util) => {
       url: url,
       view: view,
       favicon: favicon,
-      engine: 'google',
-      title: title
+      engine: "google",
+      title: title,
     });
 
     tablist.setActiveTab(id);
     pushHistory(url);
 
-    view.webContents.on('context-menu', (e, params) => {
+    view.webContents.on("context-menu", (e, params) => {
       const menu = buildChromeContextMenu({
         params: params,
         webContents: view.webContents,
         openLink: (url, disposition) => {
           view.webContents.loadURL(url);
-        }
-      })
+        },
+      });
       menu.popup();
-    })
+    });
 
-    view.webContents.on('page-title-updated', (_, title) => {
+    view.webContents.on("page-title-updated", (_, title) => {
       if (title) {
         tablist.setTabTitle(id, title);
-        event.reply('receive-tabs', tablist.friendlyTablist);
-      } 
-    })
-
-    view.webContents.on('page-favicon-updated', (_, favicons) => {
-      if (favicons.length) {
-        favicon = favicons[0];
-        tablist.setTabFavicon(id, favicon);
-        event.reply('receive-tabs', tablist.friendlyTablist);
+        event.reply("receive-tabs", tablist.friendlyTablist);
       }
     });
 
-    view.webContents.on('did-navigate', (_, url) => {
-      tablist.setTabURL(id, url);
-      event.reply('receive-tabs', tablist.friendlyTablist);
+    view.webContents.on("page-favicon-updated", (_, favicons) => {
+      if (favicons.length) {
+        favicon = favicons[0];
+        tablist.setTabFavicon(id, favicon);
+        event.reply("receive-tabs", tablist.friendlyTablist);
+      }
     });
 
-    view.webContents.on('did-navigate-in-page', (_, url) => {
+    view.webContents.on("did-navigate", (_, url) => {
       tablist.setTabURL(id, url);
-      event.reply('receive-tabs', tablist.friendlyTablist);
-    })
+      event.reply("receive-tabs", tablist.friendlyTablist);
+    });
 
-    event.reply('receive-tabs', tablist.friendlyTablist);
+    view.webContents.on("did-navigate-in-page", (_, url) => {
+      tablist.setTabURL(id, url);
+      event.reply("receive-tabs", tablist.friendlyTablist);
+    });
 
+    event.reply("receive-tabs", tablist.friendlyTablist);
   });
 
-  ipcMain.on('set-active-tab', (event, id) => {
+  ipcMain.on("set-active-tab", (event, id) => {
     tablist.setActiveTab(id);
-    event.reply('receive-tabs', tablist.friendlyTablist);
+    event.reply("receive-tabs", tablist.friendlyTablist);
   });
 
-  ipcMain.on('set-active-tab-url', (event, { id, url, engine }) => {
-
+  ipcMain.on("set-active-tab-url", (event, { id, url, engine }) => {
     let formattedUrl;
 
-    checkURL(url)
+    checkURL(url);
     if (checkURL(url)) {
-      if (!url.startsWith('http://') && !url.startsWith('https://')) {
-        formattedUrl = `https://${url}`
+      if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        formattedUrl = `https://${url}`;
       } else {
         formattedUrl = url;
       }
     } else {
-      formattedUrl = engines.find(el => el.name === engine).searchURL + url.replace(' ', '+')
+      formattedUrl =
+        engines.find((el) => el.name === engine).searchURL +
+        url.replace(" ", "+");
     }
 
     pushHistory(formattedUrl);
 
-    tablist.setActiveTabURL(id, formattedUrl)
-    event.reply('receive-tabs', tablist.friendlyTablist);
-
+    tablist.setActiveTabURL(id, formattedUrl);
+    event.reply("receive-tabs", tablist.friendlyTablist);
   });
 
-  ipcMain.on('set-active-tab-engine', (event, engine) => {
+  ipcMain.on("set-active-tab-engine", (event, engine) => {
     tablist.setActiveTabEngine(engine);
-    event.reply('receive-tabs', tablist.friendlyTablist);
-  })
-
-  ipcMain.on('reload-tab', () => {
-    tablist.reloadTab();
-  })
-
-  ipcMain.on('close-tab', (event, id) => {
-    tablist.closeTab(id);
-    event.reply('receive-tabs', tablist.friendlyTablist);
+    event.reply("receive-tabs", tablist.friendlyTablist);
   });
 
-}
+  ipcMain.on("reload-tab", () => {
+    tablist.reloadTab();
+  });
+
+  ipcMain.on("close-tab", (event, id) => {
+    tablist.closeTab(id);
+    event.reply("receive-tabs", tablist.friendlyTablist);
+  });
+};
