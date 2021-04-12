dde22cd3917282b02ddd907137f215fe4a081987
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-12 18:35:31 -0400

    Added https/http functionality

--- src/components/toolbar/components/SearchBar.js
+++ src/components/toolbar/components/SearchBar.js
@@ -48,7 +48,15 @@ export const SearchBar = (props) => {
 
   return (
     <div className="flex flex-row items-center justify-start w-10/12 h-1/2 px-4 space-x-3 bg-night-tab-active rounded">
-      <Protocol protocol="https" />
+      <Protocol
+        protocol={
+          activeURL.startsWith("https")
+            ? "https"
+            : activeURL.startsWith("http")
+            ? "http"
+            : "insecure"
+        }
+      />
       <div className="flex flex-row items-center justify-start space-x-2">
         <img
           src={activeTab && engineIcons[activeTab.engine]}
@@ -67,10 +75,7 @@ export const SearchBar = (props) => {
         className="w-full py-4 bg-transparent focus:outline-none text-gray-400 placeholder-gray-500"
         ref={searchRef}
         placeholder="Search or enter a URL"
-        value={searchURL
-          .replace("https://", "")
-          .replace("http://", "")
-          .replace("www.", "")}
+        value={searchURL}
         onChange={handleChange}
         onKeyPress={handleKeyPress}
         onClick={(event) => event.target.select()}
