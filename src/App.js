dba64856ff3a3a1942b064deaa089082167e282b
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-11 22:45:16 -0400

    Changed BrowserRouter to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./assets/styles/tailwind.css";
 import "./assets/styles/fontawesome.css";
