0895c6bdbdcd9ae6fc44f4c9cc538a1a96c87ba3
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-13 13:02:53 -0400

    Added SPACE

--- src/components/titlebar/components/tabs/Tabs.js
+++ src/components/titlebar/components/tabs/Tabs.js
@@ -15,7 +15,7 @@ export const Tabs = (props) => {
   };
 
   return (
-    <div className="TabBar flex flex-row items-center justify-start h-full">
+    <div className="TabBar flex flex-row items-center justify-start h-full space-x-4">
       <div className="TitleControlBar relative w-10 h-full bg-night-tab border-t-2 border-r-2 border-night-tab-active rounded-tr"></div>
       <div className="flex flex-row items-center justify-start h-full mr-3">
         {props.tablist.map((tab, index) => (
