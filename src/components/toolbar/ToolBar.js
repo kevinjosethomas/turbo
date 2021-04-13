ba37af1081c9334e32cf53c7f07c33e5932cc060
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-13 13:16:39 -0400

    Added functionality to move backwards and forward

--- src/components/toolbar/ToolBar.js
+++ src/components/toolbar/ToolBar.js
@@ -6,8 +6,8 @@ export const Toolbar = (props) => {
   return (
     <div className="flex flex-row items-center justify-start w-full h-header-toolbar px-4 space-x-4 bg-night-tab border-t-2 border-night-tab-active">
       <Controls
-        onBackward={() => void 0}
-        onForward={() => void 0}
+        onBackward={() => window.ipcRenderer.send("backward-tab")}
+        onForward={() => window.ipcRenderer.send("forward-tab")}
         onReload={() => window.ipcRenderer.send("reload-tab")}
       />
       <SearchBar tablist={props.tablist} />
