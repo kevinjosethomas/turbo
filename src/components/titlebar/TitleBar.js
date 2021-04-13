f540358b533bf5773755f00d72eeb57689fd07f7
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-13 12:40:59 -0400

    Made titlebar smaller

--- src/components/titlebar/TitleBar.js
+++ src/components/titlebar/TitleBar.js
@@ -3,7 +3,7 @@ import { Actions } from "./components/Actions";
 
 export const Titlebar = (props) => {
   return (
-    <div className="TitleBar flex flex-row items-center justify-between w-full h-header-titlebar bg-night-mare">
+    <div className="TitleBar flex flex-row items-end justify-between w-full h-header-titlebar bg-night-mare">
       <Tabs tablist={props.tablist} />
       <Actions isMaximized={props.isMaximized} />
     </div>
