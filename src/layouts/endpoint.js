2754cfc278247a30481b6ae95075b559b8cedfa1
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-13 14:51:13 -0400

    Added response rendering

--- src/layouts/endpoint.js
+++ src/layouts/endpoint.js
@@ -1,6 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 export const Endpoint = (props) => {
   const { method } = useParams();
-  return <div>Hello</div>;
+  const endpoint = useQuery().get("endpoint");
+  const [response, setResponse] = useState("Sending request");
+
+  useEffect(() => {
+    const responseListener = (_, res) => {
+      setResponse(res.data);
+      console.log("done");
+    };
+    window.ipcRenderer.on("receive-request", responseListener);
+
+    window.ipcRenderer.send("send-request", {
+      method: method,
+      endpoint: endpoint,
+    });
+  }, []);
+
+  return (
+    <div>
+      <span>{JSON.stringify(response)}</span>
+    </div>
+  );
 };
