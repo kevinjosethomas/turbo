0a5d10fe8a1275edd9c85d99439f930e571a1aea
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-21 21:17:31 -0400

    Added check for protocol

--- src/server/utility/methods.js
+++ src/server/utility/methods.js
@@ -1,6 +1,12 @@
 const tlds = require("tlds");
 
-exports.checkURL = (url) => {
+exports.checkURL = (rawURL) => {
+  let url = rawURL.toLowerCase();
+
+  if (url.startsWith("https://") || url.startsWith("http://")) {
+    return true;
+  }
+
   const splitURL = url.split(".");
   const tld = splitURL[splitURL.length - 1].split("/")[0].split("?")[0];
 
