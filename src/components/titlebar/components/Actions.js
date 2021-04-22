2a4d6d6f5b6ce8479b60697a903f250c723faeaa
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-21 21:00:55 -0400

    Added MacOS icons for all OSes

--- src/components/titlebar/components/Actions.js
+++ src/components/titlebar/components/Actions.js
@@ -1,42 +1,35 @@
+import close from "../../../assets/icons/titlebar/osx_close.svg";
+import maximize from "../../../assets/icons/titlebar/osx_maximize.svg";
+import minimize from "../../../assets/icons/titlebar/osx_minimize.svg";
+
+// onClick={() => window.ipcRenderer.send("window-change", props.type)}
+
 export const Actions = (props) => {
   return (
-    <div className="ActionBar flex flex-row items-center justify-end pr-4 space-x-2">
+    <div className="ActionBar flex flex-row items-center justify-end p-4 h-full space-x-3">
       <Action type="minimize" />
-      {props.isMaximized ? (
-        <Action type="restore" />
-      ) : (
-        <Action type="maximize" />
-      )}
+      <Action type="maximize" />
       <Action type="close" />
     </div>
   );
 };
 
 export const Action = (props) => {
-  let icon;
-  switch (props.type) {
-    case "close":
-      icon = <i className="far fa-times text-lg"></i>;
-      break;
-    case "restore":
-      icon = <i className="far fa-window-restore text-lg" />;
-      break;
-    case "maximize":
-      icon = <i className="far fa-window-maximize text-lg"></i>;
-      break;
-    case "minimize":
-      icon = <i className="far fa-window-minimize text-lg" />;
-      break;
-  }
+  const icon =
+    props.type === "close"
+      ? close
+      : props.type === "maximize"
+      ? maximize
+      : props.type === "minimize"
+      ? minimize
+      : null;
 
   return (
-    <div
-      className={`flex flex-row items-center justify-center w-10 h-10 cursor-pointer rounded ${
-        props.type === "close" ? "hover:bg-red-800" : "hover:bg-night-700"
-      } text-night-sky-dusk hover:text-night-sky-noon hover:bg-night-tab-active transition duration-300`}
+    <img
+      src={icon}
+      className="w-3 filter hover:brightness-75 transition duration-300"
+      draggable="false"
       onClick={() => window.ipcRenderer.send("window-change", props.type)}
-    >
-      {icon}
-    </div>
+    />
   );
 };
