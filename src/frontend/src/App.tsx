75ae5ed02e39f249d9bd66793db5a3067228cae5
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-24 12:53:31 -0400

    🌊 Added support for TailwindCSS

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import "./styles/tailwind.css";
+
 const App = () => {
   return <Fragment></Fragment>;
 };
---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
