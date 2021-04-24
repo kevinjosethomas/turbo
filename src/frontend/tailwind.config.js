f17ccc30133a3870e57db73f5dc0b3c2cf44ea12
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-24 13:22:09 -0400

    🖌 Added customizable colors for TailwindCSS

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        back: {
+          10: "var(--color-back-10)",
+          20: "var(--color-back-20)",
+          30: "var(--color-back-30)",
+        },
+        fore: {
+          10: "var(--color-fore-10)",
+          20: "var(--color-fore-20)",
+          30: "var(--color-fore-30)",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
