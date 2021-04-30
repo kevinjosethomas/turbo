5315c510806f7c06a1ba2ecf99ae392c4e4a3110
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-30 13:47:37 -0400

    🐉 Added draggable tab functionality!

--- src/frontend/src/components/browser/titlebar/components/Tabs.tsx
+++ src/frontend/src/components/browser/titlebar/components/Tabs.tsx
@@ -1,30 +1,52 @@
+import arrayMove from "array-move";
 import { FC, useRef } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-import { TabProps, TabComponentProps } from "../../../../lib/types";
+import { TabProps } from "../../../../lib/types";
 import LoadingFavicon from "../../../../assets/icons/browser/loading-favicon.gif";
 import MissingFavicon from "../../../../assets/icons/browser/missing-favicon.svg";
 
 interface TabsProps {
   tablist: TabProps[];
   setTablist: any;
+  innerRef?: any;
 }
 
-const Bar: FC<TabsProps> = (props) => {
+interface TabComponentProps {
+  id: number;
+  url: string;
+  friendlyUrl: string;
+  title?: string;
+  favicon?: string;
+  active?: boolean;
+  engine?: string;
+  suffix: boolean;
+  tablist: TabProps[];
+  setTablist: any;
+  index: number;
+  innerRef?: any;
+  draggableProps?: any;
+  dragHandleProps?: any;
+}
+
+const Container: FC<TabsProps> = (props) => {
   const onDragEnd = (result: any) => {
-    return result;
+    window.ipc.tab.emitters.order(
+      result.source.index,
+      result.destination.index
+    );
   };
 
   return (
     <div className="flex flex-row items-center justify-start h-full space-x-4">
       <Search />
-      <DragDropContext>
-        <Droppable>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId={"tabs"} direction="horizontal">
           {(provided: any) => (
             <Tabs
               {...props}
-              innerRef={provided.innerRef}
               {...provided.droppableProps}
+              innerRef={provided.innerRef}
             >
               {provided.placeholder}
             </Tabs>
@@ -36,9 +58,18 @@ const Bar: FC<TabsProps> = (props) => {
   );
 };
 
+const Search: FC = () => {
+  return (
+    <i className="fas fa-caret-circle-down text-fore-10 hover:text-fore-20 transition duration-500 window-drag-none" />
+  );
+};
+
 const Tabs: FC<TabsProps> = (props) => {
   return (
-    <div className="flex flex-row itemss-center justify-center h-full window-drag-none">
+    <div
+      ref={props.innerRef}
+      className="flex flex-row itemss-center justify-center h-full window-drag-none"
+    >
       {props.tablist.map((tab: TabProps, index: number) => (
         <TabContainer
           key={tab.id}
@@ -62,22 +93,14 @@ const Tabs: FC<TabsProps> = (props) => {
   );
 };
 
-const Search: FC = () => {
-  return (
-    <i className="fas fa-caret-circle-down text-fore-10 hover:text-fore-20 transition duration-500 window-drag-none" />
-  );
-};
-
 const TabContainer: FC<TabComponentProps> = (props) => {
-  const closeRef = useRef<HTMLDivElement>(null);
-
   return (
-    <Draggable draggableId={props.id} index={props.index}>
+    <Draggable draggableId={props.id.toString()} index={props.index}>
       {(provided: any) => (
         <Tab
           {...props}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
+          draggableProps={provided.draggableProps}
+          dragHandleProps={provided.dragHandleProps}
           innerRef={provided.innerRef}
         ></Tab>
       )}
@@ -88,21 +111,32 @@ const TabContainer: FC<TabComponentProps> = (props) => {
 const Tab: FC<TabComponentProps> = (props) => {
   const closeRef = useRef<HTMLDivElement>(null);
 
+  const onMouseDown = (event: any) => {
+    if (closeRef.current && !closeRef.current.contains(event.target)) {
+      if (props.active) {
+        return;
+      } else {
+        window.ipc.tab.emitters.active(props.id);
+      }
+    }
+  };
+
+  const onClose = (event: any) => {
+    event.stopPropagation();
+    window.ipc.tab.emitters.close(props.id);
+  };
+
   return (
     <div
-      className={`flex flex-row items-center justify-between h-full px-4 w-48 cursor-default select-none ${
+      className={`unitab flex flex-row items-center justify-between h-full px-4 w-48 select-none ${
         props.active
           ? "active-tab relative bg-back-20 rounded-t border-l-2 border-t-2 border-r-2 border-back-30"
           : `tab hover:bg-back-20 ${props.suffix ? "relative tab-suffix" : ""}`
-      } `}
-      onDragStart={(e) => e.dataTransfer.setDragImage(new Image(), 0, 0)}
-      onMouseDown={(event) => {
-        if (
-          closeRef.current &&
-          !closeRef.current.contains((event as any).target)
-        )
-          !props.active ? window.ipc.tab.emitters.active(props.id) : void 0;
-      }}
+      }`}
+      ref={props.innerRef}
+      onMouseDown={onMouseDown}
+      {...props.draggableProps}
+      {...props.dragHandleProps}
     >
       <div className="flex flex-row items-center justify-center space-x-2 w-full max-w-xxs">
         <img
@@ -123,10 +157,7 @@ const Tab: FC<TabComponentProps> = (props) => {
       <div
         className="flex flex-col items-center justify-center w-5 h-5 min-w-max rounded text-fore-10 hover:text-fore-20 hover:bg-back-30 transition duration-500"
         ref={closeRef}
-        onClick={(e) => {
-          e.stopPropagation();
-          window.ipc.tab.emitters.close(props.id);
-        }}
+        onClick={onClose}
       >
         <i className="far fa-times text-sm" />
       </div>
@@ -145,4 +176,4 @@ const NewTab: FC = () => {
   );
 };
 
-export default Bar;
+export default Container;
--- src/frontend/src/lib/types.ts
+++ src/frontend/src/lib/types.ts
@@ -7,17 +7,3 @@ export interface TabProps {
   active?: boolean;
   engine?: string;
 }
-
-export interface TabComponentProps {
-  id: number;
-  url: string;
-  friendlyUrl: string;
-  title?: string;
-  favicon?: string;
-  active?: boolean;
-  engine?: string;
-  suffix: boolean;
-  tablist: TabProps[];
-  setTablist: any;
-  index: number;
-}
